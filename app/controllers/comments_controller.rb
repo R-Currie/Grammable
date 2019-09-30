b5daf53a1804d83a6547b3c4923d3ea15c1b0917
Robert Currie <currierobertm@gmail.com>
2019-09-30 23:23:57 +0000

    Comments dont work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,18 @@
 class CommentsController < ApplicationController
     before_action :authenticate_user!, only: [:create]
+    
     def create
       @post = Post.find_by_id(params[:post_id])
+      return render_not_found if @post.blank?
       @post.comments.create(comment_params.merge(user: current_user))
       redirect_to root_path
     end
   
     private
+
+    def render_not_found(status=:not_found)
+        render plain: "#{status.to_s.titleize}", status: status
+    end
   
     def comment_params
       params.require(:comment).permit(:message)
